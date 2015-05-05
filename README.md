933685db1604101c60066bdeaea0afd06824d284
Alexander-Taran <Alexander-Taran@users.noreply.github.com>
2015-05-05 16:16:21 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TranslitAndSlugify
+A simple utility that converts Cyrrilic Russian string to Translit and another one that strips non alphanumeric symbols from a string for a url slug.
---
+++ src/Cyrillic2Slug/Slugger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Cyrillic2Slug
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+
+    public static class Slugger
+    {
+
+        public static string ToSlug(this string s)
+        {
+
+            return Regex.Replace(new string(s.Select(
+                c => char.IsLetterOrDigit(c) ? c : '-'
+                ).ToArray()).Trim('-'), "-+", "-").Translit();
+
+        }
+    }
+}
